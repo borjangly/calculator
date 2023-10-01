583d0f0dbdf9fd0d8cb6241f175912ddafeb59fd
borjangly <52857575+borjangly@users.noreply.github.com>
2023-10-01 08:39:03 -0400

    not actually working on links lmao

--- calcs.py
+++ calcs.py
@@ -1,5 +1,6 @@
 # Some random stat calculations
 import level
+import math
 
 # Calculates the final damage increase coming off an increase of 1 attack
 def attack_increase(base_attack, attack_percent, final_attack):
@@ -28,7 +29,6 @@ def stats(character_level, stat_object):
     ap = level.ability_points(character_level)
     print(ap)
     # see below
-    # what is the last 40?
     # 50 2nd job skills
     # 98 monster life
     # 28 from v skills rope + dse
@@ -36,9 +36,10 @@ def stats(character_level, stat_object):
     # 70 from link skill
     # 20 pb title
     # 5 from beginner alliance skill
-    base_stat = ap * 1 + stat_object["str"] + 50 + (20 + 20 + 6 + 5 + 10 + 5 + 5 + 6 + 14 + 7) + (23 + 5) + 25 + 70 + 20 + 5 + 40
+    # 40 from lvl 20 passive guild skill
+    base_stat = math.floor(ap * 1) + stat_object["str"] + 50 + (20 + 20 + 6 + 5 + 10 + 5 + 5 + 6 + 14 + 7) + (23 + 5) + 25 + 70 + 20 + 5 + 40
     print(base_stat)
     # hardcoding in some extra stats that i'm too lazy to calculate for now
-    total_stat = base_stat * (1 + (stat_object["str%"] + stat_object["all_stat%"])/100) + stat_object["final_str"] + 13200 + 6500 + 120
+    total_stat = math.floor(base_stat * (1 + (stat_object["str%"] + stat_object["all_stat%"])/100)) + stat_object["final_str"] + 13200 + 6500 + 120
 
     return total_stat
